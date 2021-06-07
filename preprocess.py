516f46e198a507812c53503fa80b8a542bc9000d
nahrusruno <42121958+nahrusruno@users.noreply.github.com>
2021-06-07 23:03:47 +0300

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -8,8 +8,10 @@ Created on Sat Jun  5 22:57:03 2021
 
 
 from scipy.signal import butter, lfilter, sosfilt
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy import interpolate
 from generateDataset import get_csv
 
 
@@ -60,6 +62,43 @@ def ewma_raw_EMGs():
             windowed_df.loc[i,'{}'.format(j)]  = onur_data.ewm(span=5, adjust=True).mean().values      
     return windowed_df
 
+def interpolator(data, new_sample_size):
+    
+    x = np.arange(0, np.shape(data)[0])
+    y = data
+    #print("x'in boyutu: " + str(np.shape(x)))
+    #print("y'nin boyutu: " + str(np.shape(y)))
+    f = interpolate.interp1d(x, y,axis=0, kind='cubic', bounds_error=False, fill_value= "extrapolate")
+    increment = np.shape(data)[0]/new_sample_size
+    xnew = np.arange(0, np.shape(data)[0], increment)
+    ynew = f(xnew)   # use interpolation function returned by `interp1d`
+
+    return ynew
+
+def filter_EMGs_with_interpolation() :   
+    datam = get_csv() 
+    filtered_df = pd.DataFrame(columns=['TA', 'SO', 'GAM', 'PL', 'RF', 'VM', 'BF', 'GM'])    
+    for i in range(len(datam)):
+        fs = datam.loc[i,'EMGFreq']
+        for j in filtered_df.columns:
+ 
+            filtered_signal = butter_bandpass_filter(datam.loc[i,'{}'.format(j)],10,200,fs)
+            filtered_df.loc[i,'{}'.format(j)] = interpolator(data=filtered_signal, new_sample_size=1000)
+
+    return filtered_df
+
+def ewma_raw_EMGs_with_interpolation():    
+    datam = get_csv() 
+    windowed_df = pd.DataFrame(columns=['TA', 'SO', 'GAM', 'PL', 'RF', 'VM', 'BF', 'GM'])
+    for i in range(len(datam)):
+        for j in windowed_df.columns:
+
+            onur_data = pd.DataFrame(data=datam.loc[i,'{}'.format(j)])
+            windowed_df.loc[i,'{}'.format(j)]  = interpolator(data=onur_data.ewm(span=5, adjust=True).mean().values, new_sample_size=1000)    
+            
+    return windowed_df
+
+
 def concatingDataframe(filtered_df):
     
     datam = get_csv() 
@@ -67,19 +106,33 @@ def concatingDataframe(filtered_df):
     
     return united_df
 
+#interpolator(data=filtered_windowed.loc[1,'TA'], new_sample_size=1000)
 
-# kelebek1 = onur_data.ewm(span=5, adjust=True).mean()
-# kelebek2 = onur_data.ewm(span=10, adjust=True).mean()
-# kelebek3 = onur_data.ewm(span=15, adjust=True).mean()
-# kelebek4 = onur_data.ewm(span=20, adjust=True).mean()
-# plt.plot(onur_data)
-# plt.plot(kelebek1.values)
-# plt.plot(kelebek2.values)
-# plt.plot(kelebek3.values)
-# plt.plot(kelebek4.values)
-# plt.legend(['original','1','2','3','4'])
-# plt.show()
 
+concatingDataframe(filter_EMGs_with_interpolation)
+
+"""
+## PLOT of differences wrt changing alpha(span) number
+
+onur = datam.loc[1,'TA']
+onur_data = pd.DataFrame(data=onur)
+
+kelebek1 = onur_data.ewm(span=5, adjust=True).mean()
+kelebek2 = onur_data.ewm(span=10, adjust=True).mean()
+kelebek3 = onur_data.ewm(span=15, adjust=True).mean()
+kelebek4 = onur_data.ewm(span=20, adjust=True).mean()
+plt.plot(onur_data)
+plt.plot(kelebek1.values)
+plt.plot(kelebek2.values)
+plt.plot(kelebek3.values)
+plt.plot(kelebek4.values)
+plt.legend(['original','1','2','3','4'])
+plt.show()
+"""
+
+
+"""
+## PLOTs of differences between data transformations
 datam = get_csv()
 filtered_df = filter_EMGs()
 raw_windowed = ewma_raw_EMGs()
@@ -106,7 +159,7 @@ plt.plot(raw_window_unique)
 plt.plot(filtered_window_unique)
 plt.legend(['original','filtered','raw_window_unique','filtered_window_unique'])
 plt.show()
-
+"""
 
 
 
@@ -131,22 +184,26 @@ plt.show()
 
 # filtered_df = pd.concat([filtered_df,datam.iloc[:,0:9]],axis=1)
 
-        # if j != 'Task':
-        #     filtered_signal = butter_bandpass_filter(datam.loc[i,'{}'.format(j)],10,200,fs)
-            
-        #     filtered_df.loc[i,'{}'.format(j)] = filtered_signal 
-            
-        #     df_data = pd.DataFrame(data=filtered_signal)
-        #     df_data.ewm(span=10, adjust=True).mean()
-        #     ewma_df.loc[i,'{}'.format(j)] = df_data.values
 
 
-# onur = datam.loc[1,'TA']
-# onur_data = pd.DataFrame(data=onur)
-# windowed_data = onur_data.ewm(span=10, adjust=True).mean().values
 
+"""
+## EDA OF LENGTH OF EMG DATA CODE BLOCK ##
 
-#surhan = filtered_df.loc[1,'TA']
-#surhan_data = pd.DataFrame(data=surhan)
-# windowed_data = surhan_data.ewm(span=10, adjust=True).mean().values
+len_df = pd.DataFrame(columns=['length'])
+import numpy as np
+summation = 0
+for i in range(np.shape(filtered_df.loc[:,'TA'])[0]):
+    current_len = np.shape(filtered_df.loc[:,'TA'][i])[0]
+    len_df.loc[i,'length'] = current_len
+    summation += np.shape(filtered_df.loc[:,'TA'][i])[0]
+avg_len = summation/np.shape(filtered_df.loc[:,'TA'])[0]
+
+import seaborn as sns
+
+sns.histplot(data=len_df, x='length')
+sns.displot(data=len_df, x='length')
+
+"""
+
 
